7d1a3e69548b0e894dfa007d768d3b956078f54a
Jonatas <jonatasoliveira1234@gmail.com>
2023-06-03 19:55:18 -0300

    Simple Hot Reload config with Docker and Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true
-    }),
-    UserModule,
-    AuthModule
+    ConfigModule.forRoot({isGlobal: true})
   ],
   controllers: [AppController],
   providers: [AppService],
